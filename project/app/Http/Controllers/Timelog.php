11457959ddb744b7344df7963e47852fed986412
Alex Wharff <git@maxxwv.com>
2019-09-21 16:00:38 -0400

    Timelog hydration

--- project/app/Http/Controllers/Timelog.php
+++ project/app/Http/Controllers/Timelog.php
@@ -3,8 +3,25 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use GuzzleHttp\Client;
+use App\Timelog as TimelogModel;
 
 class Timelog extends Controller
 {
-    //
+    public function hydrate(Request $request)
+    {
+        $client = new Client();
+        $logs = $client->request('GET', 'https://my-json-server.typicode.com/bomoko/algm_assessment/timelogs');
+        if($logs->getStatusCode() == 200){
+            TimelogModel::truncate();
+            foreach(json_decode($logs->getBody()->getContents()) as $log){
+                $l = new TimelogModel;
+                $l->id = $log->id;
+                $l->user_id = $log->user_id;
+                $l->issue_id = $log->issue_id;
+                $l->seconds_logged = $log->seconds_logged;
+                $l->save();
+            }
+        }
+    }
 }
