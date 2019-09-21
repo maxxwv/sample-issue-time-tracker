6f838c3ce4a6ce055957d4c2926c320eb4160566
Alex Wharff <git@maxxwv.com>
2019-09-21 13:49:05 -0400

    API endpoint working for issue-component hydration

--- project/app/Http/Controllers/IssueComponent.php
+++ project/app/Http/Controllers/IssueComponent.php
@@ -9,11 +9,25 @@ use App\IssueComponent as ICModel;
 
 class IssueComponent extends Controller
 {
+    /**
+     * We call out to the JSON endpoint to get the data to insert into the local database.
+     * Note that if this is run multiple times, we'll throw an integrity error due to the
+     * inclusion of ID in the seed data, so I'm truncating both the Issue and IssueComponent
+     * tables.
+     *
+     * It might be a bit of an overstep, but I'm also seeding both tables from this single
+     * controller as it seems a bit silly not to, honestly.
+     *
+     * @param Request $request
+     * @return void
+     */
     public function hydrate(Request $request)
     {
         $client = new Client();
         $issues = $client->request('GET', 'https://my-json-server.typicode.com/bomoko/algm_assessment/issues');
         if($issues->getStatusCode() == 200){
+            Issue::truncate();
+            ICModel::truncate();
             foreach(json_decode($issues->getBody()->getContents()) as $issue){
                 $i = new Issue;
                 $i->id = $issue->id;
--- project/routes/api.php
+++ project/routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Http\Request;
+use App\Http\Controllers\IssueComponent;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +14,10 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
+Route::get('/user', function (Request $request) {
     return $request->user();
+});
+Route::get('/hydrate/issuecomponents', function(Request $request) {
+    $ic = new IssueComponent;
+    return $ic->hydrate($request);
 });
